c9ca2e20a7049cc7a6f7bae1e0371015a21eece9
Andriy Kulyk <56300415+Andrew10x@users.noreply.github.com>
2021-03-13 21:35:39 +0200

    jkj

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ myObj[obj]              = "Object";
 myObj[""]               = "Even an empty string";
 
 console.log(myObj);
+
+
+
+var myObj = new Object(),
+    str = "mytring",
+    rand = Math.random(),
+    obj = new Object();
+
+myObj.type              = "Dot syntax";
+myObj["date created"]   = "String with space";
+myObj[str]              = "String value";
+myObj[rand]             = "Random Number";
+myObj[obj]              = "Object";
+myObj[""]               = "Even an empty string";
+
+console.log(myObj);
